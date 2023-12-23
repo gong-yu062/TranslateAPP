31a180f7ed4bca84b72b76e4c9644ab9de9df2cd
Zoey Yang <zoeyyang@Zoeys-MacBook-Air.local>
2023-12-23 01:13:09 +0000

    remove useless code

--- SeleniumTest/UnitTest1.cs
+++ SeleniumTest/UnitTest1.cs
@@ -1,51 +1,46 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
-namespace SeleniumTest
+﻿namespace SeleniumTest
 {
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+
     [TestClass]
     public class SeleniumTest1
     {
-        //private readonly string webAppUri = "http://localhost:5026/";
         private IWebDriver? driver;
 
         [TestInitialize]
         public void Initialize()
         {
             // Setup - Initialize WebDriver
-            driver = new ChromeDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
         [TestMethod]
         public void TestTranslationFeature()
         {
-            if (driver == null)
+            if (this.driver == null)
             {
                 Assert.Fail("WebDriver is not initialized.");
                 return; // Exit the method if driver is null
             }
 
             // Navigate to the Translate page
-            driver.Navigate().GoToUrl("http://localhost:5026/translate"); 
+            this.driver.Navigate().GoToUrl("http://localhost:5026");
 
             // Find elements
-            var textArea = driver.FindElement(By.Id("textToTranslate"));
-            var targetLanguageSelect = driver.FindElement(By.Id("targetLanguage"));
-            var submitButton = driver.FindElement(By.TagName("button"));
+            var textArea = this.driver.FindElement(By.Id("textToTranslate"));
+            var targetLanguageSelect = this.driver.FindElement(By.Id("targetLanguage"));
+            var submitButton = this.driver.FindElement(By.TagName("button"));
 
             // Perform actions
             textArea.SendKeys("Hello");
-            targetLanguageSelect.SendKeys("Spanish"); 
+            targetLanguageSelect.SendKeys("Spanish");
             submitButton.Click();
 
             // Wait and assert for the translation result
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
             var resultElement = wait.Until(drv => drv.FindElement(By.ClassName("alert")));
 
             // Assertions
@@ -55,14 +50,16 @@ namespace SeleniumTest
             // Additional assertions and interactions can be added here
         }
 
-
         [TestCleanup]
         public void Cleanup()
         {
             // Close the WebDriver
-             driver.Quit();
+            driver.Quit();
         }
 
-
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
